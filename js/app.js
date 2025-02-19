2138ff81445fb9d209f2ef7528875cfbb4921a9b
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-02-19 09:24:58 +0700

    Js snow animation

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ const updateStats = () => {
   const completeTasks = tasks.filter((task) => task.completed).length;
   const totalTasks = tasks.length;
   let progress;
-  if (totalTasks == 0) {
+  if (totalTasks === 0) {
     progress = 0;
   } else {
     progress = (completeTasks / totalTasks) * 100;
@@ -73,6 +73,10 @@ const updateStats = () => {
   document.getElementById(
     "numbers"
   ).innerHTML = `${completeTasks} / ${totalTasks}`;
+
+  if (tasks.length && completeTasks === totalTasks) {
+    snow();
+  }
 };
 
 const updateTaskList = () => {
@@ -111,3 +115,41 @@ document.getElementById("newTask").addEventListener("click", function (e) {
 
   addTask();
 });
+
+const snow = () => {
+  const duration = 3 * 1000,
+  animationEnd = Date.now() + duration;
+
+let skew = 1;
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+(function frame() {
+  const timeLeft = animationEnd - Date.now(),
+    ticks = Math.max(200, 500 * (timeLeft / duration));
+
+  skew = Math.max(0.8, skew - 0.001);
+
+  confetti({
+    particleCount: 1,
+    startVelocity: 0,
+    ticks: ticks,
+    origin: {
+      x: Math.random(),
+      // since particles fall down, skew start toward the top
+      y: Math.random() * skew - 0.2,
+    },
+    colors: ["#ffffff"],
+    shapes: ["circle"],
+    gravity: randomInRange(0.4, 0.6),
+    scalar: randomInRange(0.4, 1),
+    drift: randomInRange(-0.4, 0.4),
+  });
+
+  if (timeLeft > 0) {
+    requestAnimationFrame(frame);
+  }
+})();
+}
